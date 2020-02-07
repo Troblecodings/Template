1263ea36c87a66f7f4ae5a0b30ab26ae604cf1da
MrTroble <nifroh56@gmail.com>
2020-02-07 18:37:40 +0100

    Fixed issue with rendering

--- Template/TGMain.cpp
+++ Template/TGMain.cpp
@@ -3,6 +3,7 @@
 #include <gamecontent/PlayerController.hpp>
 #include <gamecontent/Actor.hpp>
 #include <pipeline/buffer/Texturebuffer.hpp>
+#include <gamecontent/camera/2DCamera.hpp>
 
 using namespace tge::gmc;
 using namespace tge::tex;
@@ -12,8 +13,27 @@ int main() {
 
 	playercontroller = [](Input* in){};
 
+	TopDownCamera camera;
+	camera.positionx = 0;
+	camera.positiony = 0;
+	camera.screenwidth = tge::win::mainWindowWidth;
+	camera.screenheight = tge::win::mainWindowHeight;
+	setTopDownCamera(&camera);
+
+	SamplerInputInfo samplerInputInfo;
+	samplerInputInfo.uSamplerMode = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+	samplerInputInfo.vSamplerMode = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+	samplerInputInfo.filterMagnification = VK_FILTER_NEAREST;
+	samplerInputInfo.filterMignification = VK_FILTER_NEAREST;
+
+	Sampler sampler;
+	SamplerBindingInfo samplerbindinginfo;
+	createSampler(samplerInputInfo, &sampler, &samplerbindinginfo);
+	bindSampler(samplerbindinginfo, 0);
+	bindSampler(samplerbindinginfo, 1);
+
 	TextureInputInfo texture;
-	texture.data = new uint8_t[4]{ 1, 1, 1, 1};
+	texture.data = new uint8_t[4]{ 255, 255, 255, 255 };
 	texture.x = 1;
 	texture.y = 1;
 	texture.comp = 1;
@@ -21,11 +41,15 @@ int main() {
 	Texture tex;
 	TextureBindingInfo bindinginfo[2048];
 	createTextures(&texture, 1, &tex, bindinginfo);
-	bindTextures(bindinginfo, 1, 0);
+	bindTextures(bindinginfo, 2048, 0);
+	bindTextures(bindinginfo, 2048, 1);
 
 	createdMaterials = new Material[1];
 	createdMaterials[0].color = { 1, 0, 0, 1};
 	createdMaterials[0].diffuseTexture = 0;
+
+	uint32_t actorIdx[] = { 0, 1, 2, 0, 2, 3 };
+	float actorVertex[] = { -0.5, -0.5, 0, 0, 0.5, -0.5, 1, 0, 0.5, 0.5, 1, 1, -0.5, 0.5, 0, 1 };
 
 	ActorInputInfo actor;
 	actor.pProperties.localTransform = {
@@ -36,10 +60,10 @@ int main() {
 	};
 	actor.pProperties.material = 0;
 	actor.pProperties.layer = 0;
-	actor.indexCount = 3;
-	actor.pIndices = new uint32_t[3]{ 1, 2, 3};
-	actor.vertexCount = 3;
-	actor.pVertices = (uint8_t*)new glm::vec4[3]{ { 0, 0, 0, 0}, { 0, 1, 0, 1}, { 1, 1, 1, 1} };
+	actor.indexCount = 6;
+	actor.pIndices = actorIdx;
+	actor.vertexCount = 4;
+	actor.pVertices = (uint8_t*)actorVertex;
 	createActor(&actor, 1);
 
 	startTGEngine();
